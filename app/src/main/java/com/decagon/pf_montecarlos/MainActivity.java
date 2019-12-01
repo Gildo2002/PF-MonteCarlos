9e7725f6c3f9191808f05c648646a1b0a1b99d5a
Decagon <chijin2002@gmail.com>
2019-11-30 19:11:53 -0500

    Primer Commit

---
+++ app/src/main/java/com/decagon/pf_montecarlos/MainActivity.java
@@ -0,0 +1,13 @@
+package com.decagon.pf_montecarlos;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import org.mariuszgromada.math.mxparser.*;
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
